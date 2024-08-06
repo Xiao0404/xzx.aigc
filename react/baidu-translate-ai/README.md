839e97c0eedd4290dff7d7900acd5cb12bbbb964
萧正轩 <332722315@qq.com>
2024-08-06 12:42:00 +0800

    '下载了本地大模型'

--- react/baidu-translate-ai/README.md
+++ react/baidu-translate-ai/README.md
@@ -8,6 +8,13 @@
 - coze agent 开发，也调用过接口完成项目中的AI应用
 - 了解OpenAI 等LLM接口，完成过文生成图，语义搜索等
 - AI带来了全新的开发体验，前端工程师需要实施AI应用用户体验
+    - prompt engineer
+        - 角色 role
+        - 清晰描述任务
+        - 分布，交给多个工作流
+        - 可以做什么，不做什么
+        - 给大模型一些思考时间
+    - button  disabled  
 - AI 端模型能力是一个风口，了解transformer.js
 
 
@@ -19,7 +26,8 @@
         - vue 是 template/script/css 三段写法  .vue
     函数就是对象、
     React(函数式组件)/Vue 函数式编程 setup
-- react 坚持不用v-model 双向绑定  性能不好
+- react 坚持不用v-model 双向绑定  性能不好   单向数据流
+    vue  v-model  双向绑定
 - MVVM + 组件化思想 + 函数式编程
     - vue 优势是 API 简好上手  setup (ref,reactive definePops) v-for/v-if
     - react 优势是 纯JS风格  函数的参数解构props  map  缺点是上手难
@@ -32,10 +40,51 @@
     react 在 hook 上很激进 ，vue 借鉴了 hook 思想
     - useState  引入一个响应式状态(ref)
         参数是默认值，返回是数组，第一个是状态，第二个是更新状态的函数
+    - useRef 
+        vue中  ref  动态绑定节点/对象
+        useRef  null ->  current
+        要快速显示组件优先，所以 web worker 的实例化推迟  useRef 动态绑定
+        worker 对象
+        - worker  组件函数内局部变量  null  访问问题
+        - useEffect  实例化  性能问题
+        - 任何地方都可以访问
+        - 卸载时回收这个对象
+    - useEffect  
+        生命周期钩子函数
+        - onMounted
+        - onUnMounted  return() => {  ...卸载功能 }
+        - onUpdated
 
 - 数据通信
     - 状态管理 pinia redux
     - 父子组件通信
+
+- html5 功能
+    - 拖拽
+    - form 表单新特性
+    - 语义化标签
+    - canvas audio
+    - 本地存储  localStorage  sessionStorage
+    - web  socket
+    - html5  file
+    - web Worker  多线程
+    - LBS  附近的位置
+    - WebGL  three.js   3D 小米汽车
+
+- Web Worker
+    - JS 因复杂任务启动worker 线程
+    - 性能优化，压缩，加密 AI ... 不做  DOM 编程
+    - 实例化 new Worker(new URL('./worker.js'),{type:'module'})
+    - 消息机制完成  主线程盒worker线程间的通信
+        事件监听
+        postMessage  发送事件
+
+- Transformer.js
+    - import   from  从cdn 加载 nlp  js 库
+    - pipeline('sentiment-analysis')  pipe()  功能函数
+        - 下载大模型   前端反馈
+    - pipe(text)  拿到结果
+        postMessage  
 
 ## 百度翻译
     - NLP 翻译任务 机器学习
--- react/baidu-translate-ai/src/App.jsx
+++ react/baidu-translate-ai/src/App.jsx
@@ -1,40 +1,87 @@
-import {useState} from 'react';
-import LanguageSelector from './/components/LanguageSelector';
+import { useState, useRef, useEffect } from 'react'
+import LanguageSelector from './components/LanguageSelector'
+import './App.css'
+
 const App = () => {
-  // useState  现在最牛逼的hook技术
-  // vue 借鉴了 react hooks 思想
-  // useReducer  vue-router , react  框架本身就内置了 hook函数
+  // useState 现在最牛逼的hook技术 
+  // vue 借鉴了 react hooks 思想 
+  // useReducer vue-router , react 框架本身就内置了hook函数
   // pinia api 
-  const [sourceLanguage,setSourceLanguage]  = useState('eng_Latn');
-  const [targetLanguage,setTargetLanguage]  = useState('zho_Hans');
-  const [input,setInput] = useState('');
-  const [output,setOutput] = useState('');
+  const [ sourceLanguage, setSourceLanguage] = useState('eng_Latn')
+  const [ targetLanguage, setTargetLanguage] = useState('zho_Hans')
+  const [ input, setInput] = useState('')
+  const [ output, setOutput] = useState('')
+  const [disabled, setDisabled] = useState(false)
+  // 
+  const worker = useRef(null); // 响应式 web worker 对象
 
-  setTimeout(()=>{
-    setSourceLanguage('zh_Hans')
-  },3000)
+  useEffect(() => {
+    // 局部 onMouted
+    // console.log('mounted')
+    if (!worker.current) {
+      worker.current = new Worker(
+        new URL("./work.js", import.meta.url), {
+          type: 'module'
+        }
+      )
+      console.log(worker.current)
 
+      worker.current.addEventListener('message', (e) => {
+        console.log(e)
+      })
+     
+    }
+    
+    return () => {
+      // onUnMounted
+      // console.log('unmounted')
+      // worker.current = null
+    }
+  })
+
+  const translate = () => {
+    setDisabled(true)
+    // ai 任务复杂， event loop 在这里就不灵了 
+    // 前端游戏， 加密， 压缩， AI  , 多线程 Web Worker 
+    // html5 浏览器提供的多线程机制（纯计算类，不能做DOM, 没有this ）
+    worker.current.postMessage({
+      text: '请帮我翻译'
+    })
+  }
+  
   return (
     <>
       <h1>Transformers.js</h1>
-      <p>来自HuggingFace 抱抱脸社区的 NLP js库</p>
-      <p>未来端模型将大放</p>
+      <p>来自HuggingFace 抱抱脸社区的NLP js库，完成常见AI 任务</p>
+      <p>未来端模型将大放异彩</p>
       <div className="container">
-       <LanguageSelector 
-        type="Source"
-        defaultLanguage={sourceLanguage}
-        onChange={x => setSourceLanguage(x.target.value)}
-       />
-       <LanguageSelector 
-        type="Source"
-        defaultLanguage={targetLanguage}
-        onChange={x => setSourceLanguage(x.target.value)}
-       />
-
-
+        <LanguageSelector 
+          type="Source"
+          defaultLanguage={sourceLanguage}
+          onChange={x => setSourceLanguage(x.target.value)}
+        />
+        <LanguageSelector 
+          type="Target"
+          defaultLanguage={targetLanguage}
+          onChange={x => setSourceLanguage(x.target.value)}
+        />
+       
       </div>
+      <div className="textbox-container">
+        <textarea 
+          value={input}
+          row={3}
+          onChange={(e) => setInput(e.target.value)}  >
+        </textarea>
+        <textarea
+          value={output}
+          row={3}
+          readOnly>
+        </textarea>
+      </div>
+      <button disabled={disabled} onClick={translate}>Translate</button>
     </>
   )
 }
 
-export default App;
+export default App 
